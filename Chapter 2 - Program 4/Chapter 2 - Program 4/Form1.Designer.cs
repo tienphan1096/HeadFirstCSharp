43f2011164abbecfa5db17a8abbd7928ef813429
Tien <tienphan1096@gmail.com>
2017-08-13 13:14:21 -0600

    Initial commit

---
+++ Chapter 2 - Program 4/Chapter 2 - Program 4/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Chapter_2___Program_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.changeText = new System.Windows.Forms.Button();
+            this.enableCheckbox = new System.Windows.Forms.CheckBox();
+            this.labelToChange = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // changeText
+            // 
+            this.changeText.AutoSize = true;
+            this.changeText.Location = new System.Drawing.Point(12, 12);
+            this.changeText.Name = "changeText";
+            this.changeText.Size = new System.Drawing.Size(203, 30);
+            this.changeText.TabIndex = 0;
+            this.changeText.Text = "Change the label if clicked";
+            this.changeText.UseVisualStyleBackColor = true;
+            this.changeText.Click += new System.EventHandler(this.changeText_Click);
+            // 
+            // enableCheckbox
+            // 
+            this.enableCheckbox.AutoSize = true;
+            this.enableCheckbox.Checked = true;
+            this.enableCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.enableCheckbox.Location = new System.Drawing.Point(275, 16);
+            this.enableCheckbox.Name = "enableCheckbox";
+            this.enableCheckbox.Size = new System.Drawing.Size(191, 24);
+            this.enableCheckbox.TabIndex = 1;
+            this.enableCheckbox.Text = "Enable label changing";
+            this.enableCheckbox.UseVisualStyleBackColor = true;
+            this.enableCheckbox.CheckedChanged += new System.EventHandler(this.enableCheckbox_CheckedChanged);
+            // 
+            // labelToChange
+            // 
+            this.labelToChange.Location = new System.Drawing.Point(12, 79);
+            this.labelToChange.Name = "labelToChange";
+            this.labelToChange.Size = new System.Drawing.Size(454, 20);
+            this.labelToChange.TabIndex = 2;
+            this.labelToChange.Text = "Press the button to change my text";
+            this.labelToChange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelToChange.Click += new System.EventHandler(this.labelToChange_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(366, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Text changed";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 108);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelToChange);
+            this.Controls.Add(this.enableCheckbox);
+            this.Controls.Add(this.changeText);
+            this.Name = "Form1";
+            this.Text = "My Desktop App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button changeText;
+        private System.Windows.Forms.CheckBox enableCheckbox;
+        private System.Windows.Forms.Label labelToChange;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ConsoleApp1/ConsoleApp1/InputConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class InputConverter
+    {
+        public double ConvertInputToNumeric(String argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expect a numeric value.");
+            return convertedNumber;
+        }
+    }
+}
